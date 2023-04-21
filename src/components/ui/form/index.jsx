172b9dc236a758a5f434e1b25568bd9eca4fb05e
Nickolay <t.e.r.r.a.nik566@gmail.com>
2023-04-21 13:21:32 +0300

    added form validation

--- src/components/ui/form/index.jsx
+++ src/components/ui/form/index.jsx
@@ -16,21 +16,43 @@ const initialInputs = [
     type: 'text',
     name: 'name',
     placeholder: 'Имя*',
+    required: {
+      value: true,
+      message: 'Укажите свое имя',
+    },
+    minLength: { value: 2, message: 'Минимум 2 символа' },
+    maxLength: { value: 30, message: 'Максимум 20 символов' },
+    pattern: { value: /^[А-Яа-яA-Za-z\s]+$/i, message: 'Только буквы' },
   },
   {
     type: 'text',
     name: 'phone',
     placeholder: 'Телефон*',
+    required: {
+      value: true,
+      message: 'Укажите свой телефон',
+    },
+    minLength: { value: 9, message: 'Минимум 9 символов' },
+    maxLength: { value: 15, message: 'Максимум 15 символов' },
+    pattern: { value: /^\d+$/, message: 'Только цифры' },
   },
   {
     type: 'text',
     name: 'email',
     placeholder: 'Почта*',
+    required: {
+      value: true,
+      message: 'Укажите свою почту',
+    },
+    minLength: { value: 5, message: 'Минимум 5 символов' },
+    maxLength: { value: 30, message: 'Максимум 30 символов' },
+    pattern: { value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'Некорректная почта' },
   },
   {
     type: 'area',
     name: 'message',
     placeholder: 'Оставьте сообщение...',
+    maxLength: { value: 150, message: 'Максимум 150 символов' },
   },
 ]
 
@@ -86,16 +108,33 @@ const FormComponent = ({
               className={styles.form_control}
             >
               {el.type === 'text' ? (
-                <input
-                  {...register(el.name)}
-                  type='text'
-                  placeholder={el.placeholder}
-                />
+                <>
+                  <input
+                    {...register(el.name, {
+                      required: el.required,
+                      minLength: el.minLength,
+                      pattern: el.pattern,
+                      maxLength: el.maxLength,
+                    })}
+                    type='text'
+                    placeholder={el.placeholder}
+                  />
+                  {errors[el.name] && (
+                    <span className={styles.error}>{errors[el.name].message}</span>
+                  )}
+                </>
               ) : (
-                <textarea
-                  {...register(el.name)}
-                  placeholder={el.placeholder}
-                ></textarea>
+                <>
+                  <textarea
+                    {...register(el.name, {
+                      maxLength: el.maxLength,
+                    })}
+                    placeholder={el.placeholder}
+                  ></textarea>
+                  {errors[el.name] && (
+                    <span className={styles.error}>{errors[el.name].message}</span>
+                  )}
+                </>
               )}
             </div>
           )
