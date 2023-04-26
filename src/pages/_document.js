e0313289574b36392b1f091354231ebb027bd288
Nickolay <t.e.r.r.a.nik566@gmail.com>
2023-04-26 20:45:51 +0300

    ya metrika added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
+  const metrikaHtml = `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+  m[i].l=1*new Date();
+  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+  (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym")`
+
   return (
     <Html lang='ru'>
-      <Head />
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              ${metrikaHtml}
+              ym(93348393, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });
+            `,
+          }}
+        />
+      </Head>
       <body id='body_id'>
         <Main />
         <NextScript />
