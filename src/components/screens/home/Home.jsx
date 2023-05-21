3a2e50b7f19333fb593b59173127005861026caf
Nickolay <t.e.r.r.a.nik566@gmail.com>
2023-05-21 07:17:25 +0300

    home page done

--- src/components/screens/home/Home.jsx
+++ src/components/screens/home/Home.jsx
@@ -758,6 +758,51 @@ const Home = ({ data }) => {
 
       <ProjectGroup data={data} />
 
+      <section
+        id='wait'
+        className={styles.wait}
+      >
+        <div className={styles.wait_title}>
+          <TitleSec
+            text={'Ждём вас в гости'}
+            size={'small'}
+          />
+        </div>
+        <div className={styles.wait_content}>
+          <div className={styles.wait_img}>
+            <Image
+              src={'/main_bg_4.jpg'}
+              fill={true}
+              alt='Ждем вас в гости | FriendlyInn Group'
+            />
+          </div>
+          <div className={styles.wait_actions}>
+            <p>
+              Приглашаем вас посетить любой из наших отелей и ресторанов, где мы в реальном времени
+              покажем, как у нас все работает. Познакомим с нашей командой и ответим на ваши вопросы
+            </p>
+            <Button
+              type={'button'}
+              className={styles.callback}
+              onClick={() => setShowModal(true)}
+            >
+              Записаться
+            </Button>
+            <Modal
+              onClose={() => setShowModal(false)}
+              show={showModal}
+            >
+              <FormComponent
+                direction='col'
+                title='Введите свои данные'
+                inputs={['name', 'phone']}
+                modal={setShowModal}
+              />
+            </Modal>
+          </div>
+        </div>
+      </section>
+
       <section className={styles.accordion}>
         <TitleSec
           text={'FAQ'}
