67463ed8b698c9ff87c30809803312b1fa0b70fa
TeeBKECH <t.e.r.r.a.nik566@gmail.com>
2024-04-04 19:59:53 +0300

    adde new bitrix widget with online chat

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,13 +30,10 @@ export default function Document() {
         />
         <script
           dangerouslySetInnerHTML={{
-            __html: `(function (w, d, u) {
-              var s = d.createElement('script')
-              s.async = true
-              s.src = u + '?' + ((Date.now() / 60000) | 0)
-              var h = d.getElementsByTagName('script')[0]
-              h.parentNode.insertBefore(s, h)
-            })(window, document, 'https://cdn-ru.bitrix24.ru/b29138824/crm/site_button/loader_3_3kvtle.js')`,
+            __html: `(function(w,d,u){
+              var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+              var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+      })(window,document,'https://cdn-ru.bitrix24.ru/b29138824/crm/site_button/loader_1_btsosa.js');`,
           }}
         />
         <script
